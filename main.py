15b7d697b92848acd85ba9674513cf66879ebcab
Marek Zelinka <mzelinka17@gmail.com>
2025-11-13 22:03:21 +0100

    get weather data

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+import os
 import sys
 
+import httpx
+from dotenv import load_dotenv
 from fastapi import FastAPI
+
+load_dotenv()
+
+apiKey = os.getenv("OPENWEATHER_API_KEY")
+if apiKey is None:
+    raise ValueError("Missing env var: apiKey")
 
 app = FastAPI()
 
@@ -12,10 +21,15 @@ async def root():
 
 @app.get("/weather/{city}")
 async def get_weather(city: str):
+    r = httpx.get(
+        f"https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={apiKey}"
+    )
+    weather = r.json()
+
     return {
         "city": city,
-        "temperature": 22,
-        "condition": "sunny",
+        "temperature": weather["main"]["temp"],
+        "condition": weather["weather"][0]["description"],
         "powered_by": "uv",
     }
 
